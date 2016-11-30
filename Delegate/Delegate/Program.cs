ab62cac1019d6e6ceeb39d60bf9910e1dfd63c3c
阿星Plus <hackxing@foxmail.com>
2016-11-30 16:57:05 +0800

    C#的委托

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    delegate int NumberChanger(int n);
+
+    class Program
+    {
+        static int num = 10;
+
+        static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            nc1(20);
+            Console.WriteLine("Value of Num:{0}", num);
+
+            MyClass mc = new MyClass();
+            NumberChanger nc2 = new NumberChanger(mc.AddNum);
+            nc2(30);
+            Console.WriteLine("Value of instance Num:{0}", mc.num);
+
+            NumberChanger nc3 = new NumberChanger(mc.MutliNum);
+            nc3(2);
+            Console.WriteLine("Value of instance Num:{0}", mc.num);
+        }
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+    }
+
+    class MyClass
+    {
+        public int num = 10;
+
+        public int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public int MutliNum(int p)
+        {
+            num *= p;
+            return num;
+        }
+    }
+}
---
+++ Delegate/Multi-Casting Delegate/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Casting_Delegate
+{
+    delegate void D(int x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D cd1 = new D(C.M1);
+            cd1(-1);
+            Console.WriteLine();
+
+            D cd2 = new D(C.M2);
+            cd2(-2);
+            Console.WriteLine();
+
+            D cd3 = cd1 + cd2;
+            cd3(10);
+            Console.WriteLine();
+
+            C c = new C();
+            D cd4 = new D(c.M3);
+            cd3 += cd4;
+            cd3(30);
+            Console.WriteLine();
+
+            cd3 += cd1;
+            cd3(20);
+            Console.WriteLine();
+
+            cd3 -= cd1;
+            cd3(40);
+            Console.WriteLine();
+
+            cd3 -= cd2;
+            cd3 -= cd4;
+            cd3(50);
+            Console.WriteLine();
+
+            cd3 -= cd1;
+            // cd3(60);//System.NullReferenceException:
+            cd3 -= cd1;
+        }
+    }
+
+    class C
+    {
+        public static void M1(int i)
+        {
+            Console.WriteLine("C.M1:" + i);
+        }
+
+        public static void M2(int i)
+        {
+            Console.WriteLine("C.M2:" + i);
+        }
+
+        public void M3(int i)
+        {
+            Console.WriteLine("C.M3:" + i);
+        }
+    }
+}
