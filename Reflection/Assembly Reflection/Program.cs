d11fa60b91587b1bfc53697b49ddc49b06257edf
阿星Plus <hackxing@foxmail.com>
2016-12-01 13:54:13 +0800

    C#的预处理指令

---
+++ Reflection/Assembly Reflection/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly objAssembly;
+            objAssembly = Assembly.Load("mscorlib,Version=4.0.0.0, Culture=neutral,PublicKeyToken=b77a5c561934e089");
+
+            Type[] types = objAssembly.GetTypes();
+
+            foreach (var t in types)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+            objAssembly = Assembly.GetExecutingAssembly();
+            Type tt = objAssembly.GetType("Reflection.Car",false,true);
+            object obj = Activator.CreateInstance(tt);
+            MethodInfo mi = tt.GetMethod("IsMoving");
+            var isMoving = (bool)mi.Invoke(obj, null);
+            if (isMoving)
+            {
+                Console.WriteLine("Is moving");
+            }
+            else
+            {
+                Console.WriteLine("Not is moving");
+            }
+        }
+    }
+
+    public class Car
+    {
+        public bool IsMoving()
+        {
+            return true;
+        }
+    }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello Reflection";
+            Type t = s.GetType();
+            Console.WriteLine(t.FullName);
+
+            Type t2 = Type.GetType("System.String", false, true);
+            Console.WriteLine(t2);
+
+            Type t3 = typeof(string);
+            Console.WriteLine(t3.FullName);
+
+            //GetMethods(t);
+
+            Console.WriteLine("Join method:{0}", t.GetMethod("Copy"));
+
+            GetMethods(t, BindingFlags.Public | BindingFlags.Instance);
+            //GetFields(),GetProperites()
+        }
+
+        public static void GetMethods(Type t,BindingFlags f)
+        {
+            MethodInfo[] mi = t.GetMethods(f);
+            foreach (MethodInfo m in mi)
+            {
+                Console.WriteLine("{0}", m.Name);
+            }
+        }
+    }
+}
