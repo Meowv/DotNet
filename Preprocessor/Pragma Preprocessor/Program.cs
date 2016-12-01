483bf69ef7647001b7440cef4e16f4c034a8f1b7
阿星Plus <hackxing@foxmail.com>
2016-12-01 13:53:52 +0800

    C#的反射机制

---
+++ Preprocessor/Pragma Preprocessor/Program.cs
@@ -0,0 +1,39 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pragma_Preprocessor
+{
+#pragma warning disable 414,3021
+    [CLSCompliant(false)]
+    class Program
+    {
+        int i = 1;
+        static void Main(string[] args)
+        {
+//#if DEBUG
+//#line 100 "Special"
+//#warning DEBUG is defined
+//#error DEBUG is defined
+//#endif
+
+//#line default
+//#warning !!!
+//#error !!!
+
+//#pragma checksum "filename" "{guid}" "check bytes"
+        }
+    }
+
+#pragma warning restore 3021
+    [CLSCompliant(false)]
+    public class D
+    {
+        int i = 1;
+        public static void M()
+        { }
+    }
+}
---
+++ Preprocessor/Preprocessor/Program.cs
@@ -0,0 +1,29 @@
+﻿#define DEBUG
+#undef DEBUG
+#define TRACE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preprocessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Hello Meowv
+            // ...
+            #endregion
+
+#if (DEBUG)
+            Console.WriteLine("Debuggimh is enable.");
+#elif(TRACE)
+            Console.WriteLine("Tracing is enabled.");
+#else
+            Console.WriteLine("Debuggimh is not enable.");
+#endif
+        }
+    }
+}
