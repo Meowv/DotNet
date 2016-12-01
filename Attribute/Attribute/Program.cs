c3124838aac29166ab5ad808e43bbe62fbf029f3
阿星Plus <hackxing@foxmail.com>
2016-12-01 13:53:04 +0800

    C#的Attribute

---
+++ Attribute/Attribute/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.Message("In Main Function");
+            Function1();
+        }
+
+        //[Obsolete("Dont't use Old Method!",true)]
+        static void Function1()
+        {
+            MyClass.Message("In Function1");
+            Function2();
+        }
+
+        static void Function2()
+        {
+            MyClass.Message("In Function2");
+        }
+    }
+
+    public class MyClass
+    {
+        [Conditional("DEBUG")]
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Attribute/Custom Attribute/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Attribute
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelpAttribute helpAttribute;
+
+            foreach (var attr in typeof(AnyClass).GetCustomAttributes(true))
+            {
+                helpAttribute = attr as HelpAttribute;
+                if (null != helpAttribute)
+                {
+                    Console.WriteLine("AnyClass Description:{0}", helpAttribute.Description);
+                }
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class HelpAttribute : Attribute
+    {
+        protected string description;
+
+        public HelpAttribute(string description_in)
+        {
+            this.description = description_in;
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    [Help("this is a do-nothing class", Name = "Meowv")]
+    public class AnyClass
+    {
+    }
+}
